58abd9fb16de9a270699a7a4a6365fe869c6ef1a
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-07-24 10:01:20 +0530

    added needed files and figured out how to use api's

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+
+  const jokes = async () => {
+    const res = await axios.get('https://icanhazdadjoke.com/',{
+      headers: {
+        'Accept' : 'application/json'
+      }
+    })
+    console.log(res)
+  }
+
+
+  return(
+    <h1>hello</h1>
+  )
 }
-
-export default App;
