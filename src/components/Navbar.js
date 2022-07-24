12a953df726c6e4a5fa4404e954659236fbbe9e0
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-07-24 10:24:07 +0530

    done with navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar(){
     return(
-        <nav>
+        <nav className='nav-bar'>
             <h1>Dad Jokes 	&#128514;</h1>
         </nav>
     )
